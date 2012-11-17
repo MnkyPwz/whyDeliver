150b1d8c7318b3479713e40fd8777716f50bede8
Jordan Leigh <AlwaysBCoding@gmail.com>
2012-11-17 03:48:07 -0600

    Added geolocation code back to model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -8,4 +8,13 @@ class Merchant < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   attr_accessible :address, :email, :lat, :long, :name, :phone
+  
+  after_commit :geolocate_address
+  
+  private
+  def geolocate_address
+    url = "http://maps.googleapis.com/maps/api/geocode/json?address=#{self.address.gsub(/\s/, '+')}&sensor=true"
+    response = JSON.parse(open(url).read)
+    self.update_attributes(:lat => response["results"][0]["geometry"]["location"]["lat"], :long => response["results"][0]["geometry"]["location"]["lng"])
+  end
 end
--- doc/geolocation_code.rb
+++
@@ -1,8 +0,0 @@
-after_commit :geolocate_address
-
-private
-def geolocate_address
-  url = "http://maps.googleapis.com/maps/api/geocode/json?address=#{self.address.gsub(/\s/, '+')}&sensor=true"
-  response = JSON.parse(open(url).read)
-  self.update_attributes(:lat => response["results"][0]["geometry"]["location"]["lat"], :long => response["results"][0]["geometry"]["location"]["lng"])
-end
