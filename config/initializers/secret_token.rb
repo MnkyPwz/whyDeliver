07e7e9c3fb54604c7ec7d2ab4f781141e05a7fd4
Jordan Leigh <AlwaysBCoding@gmail.com>
2012-11-17 00:17:48 -0600

    First commit, Initialized app with rspec, haml, and pg production db

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WhyDeliver::Application.config.secret_token = '7dc50ea040b085cfc70049f6ea8542e4ea088256cb1efcff79e3ea3ab822b5c3e23992f70af46b91816c6f9d1440f4c74ccdb359c9d40a6a14d4d03119dc790f'
