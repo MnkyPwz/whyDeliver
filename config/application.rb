2ab8393c78b2ae5e912a66fd0e3977f6ca7efb52
Greg Williams <vincentgwilliams@gmail.com>
2012-11-24 18:12:57 -0600

    stopping database initialization on precompile

--- config/application.rb
+++ config/application.rb
@@ -56,6 +56,9 @@ module WhyDeliver
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    #prevent initializing your application and connecting to the database
+    config.assets.initialize_on_precompile = false
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
